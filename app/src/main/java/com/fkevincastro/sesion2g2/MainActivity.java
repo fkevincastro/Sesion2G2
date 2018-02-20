0641b90580e23af023f3d0feb1124244099acfdc
Frank Castro <fkevin.castro@udea.edu.co>
2018-02-20 09:54:08 -0500

    version 1.1 modicado solo main activity

--- app/src/main/java/com/fkevincastro/sesion2g2/MainActivity.java
+++ app/src/main/java/com/fkevincastro/sesion2g2/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         nombre=eNombre.getText().toString();
         eCorreo.setText(nombre);
     }
-
+//hola
     public void onRadioButtonClicked(View view) {
         int id=view.getId();
         if(id==R.id.rMaculino){
